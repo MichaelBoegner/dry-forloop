50586da32a7d995fe183d9a9e1b972788176abbc
LaikaFusion <newrad4@gmail.com>
2019-03-08 15:37:28 -0500

    added switch case for length

--- index.js
+++ index.js
@@ -1,84 +1,57 @@
 exports.forDry;
 
-Array.prototype.forDry = function(cb, index=0, operator, length, iterator) {
+Array.prototype.forDry = function(cb, index = 0, operator, length, iterator) {
+  //es6 default values only trigger on undefined and not null, this will trigger on anything false (including 0)
+  operator = operator || "<=";
+  length = length || "length - 1";
+  iterator = iterator || "++";
+  //special case since 0 is considered false for the above methods
+  index == "null" ? (index = 0) : "";
 
+  switch (length) {
+    case "length - 1":
+      length = this.length - 1;
+      break;
+    case "length":
+      length = this.length;
+      break;
+    default:
+      length = parseInt(length);
+      break;
+  }
 
-        //es6 default values only trigger on undefined and not null, this will trigger on anything false (including 0)
-        operator = operator || '<=';
-        length = length||'length - 1';
-        iterator = iterator||'++';
-        //special case since 0 is considered false for the above methods
-        index == 'null'? index = 0 : "";
-
-        const operators = {
-            '>': () => {
-                    if(lengths[length] === undefined) {
-                        return index > length;
-                    } else {
-                        return index > lengths[length](); 
-                    }
-                }, 
-            '<': () => {
-                    if(lengths[length] === undefined) {
-                        return index < length;
-                    } else {
-                        return index < lengths[length](); 
-                    }
-                }, 
-            '>=': () => {
-                    if(lengths[length] === undefined) {
-                        return index >= length;
-                    } else {
-                        return index >= lengths[length](); 
-                    }
-                }, 
-            '<=': () => {
-                    if(lengths[length] === undefined) {
-                        return index <= length;
-                    } else {
-                        return index <= lengths[length](); 
-                    }
-                }, 
-            '!==': () => {
-                    if(lengths[length] === undefined) {
-                        return index !== length;
-                    } else {
-                        return index !== lengths[length](); 
-                    }
-                }, 
-            '===': () => {
-                    if(lengths[length] === undefined) {
-                        return index === length;
-                    } else {
-                        return index === lengths[length](); 
-                    }
-                },    
-        },
-        iterators = {
-            '++': () => {
-                    return index++; 
-                },
-            '--': () => {
-                    return index--; 
-                },  
-        },
-        lengths = {
-            'length - 1': () => {
-                    return this.length - 1; 
-                },
-            'length': () => {
-                    return this.length; 
-                },  
-        }; 
-    
-    for(index; operators[operator](); iterators[iterator]()) {
-        cb(this[index], index, this);
+  const operatorsObj = {
+    ">": () => {
+      return index > length;
+    },
+    "<": () => {
+      return index < length;
+    },
+    ">=": () => {
+      return index >= length;
+    },
+    "<=": () => {
+      return index <= length;
+    },
+    "!==": () => {
+      return index !== length;
+    },
+    "===": () => {
+      return index === length;
     }
+  };
+  const iteratorObj = {
+    "++": () => {
+      return index++;
+    },
+    "--": () => {
+      return index--;
+    }
+  };
 
-    return;
-}
+  for (index; operatorsObj[operator](); iteratorObj[iterator]()) {
+    cb(this[index], index, this);
+  }
 
-
-
-
-
+  return;
+};
